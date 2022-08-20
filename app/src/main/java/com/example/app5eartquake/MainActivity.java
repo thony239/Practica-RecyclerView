f2192efa595c3f28b0e7dec91de10ee7d29fa3c3
thony239 <anthony.morocho.est@tecazuay.edu.ec>
2022-08-19 20:00:01 -0500

    19/18/22

--- app/src/main/java/com/example/app5eartquake/MainActivity.java
+++ app/src/main/java/com/example/app5eartquake/MainActivity.java
@@ -1,16 +1,17 @@
 package com.example.app5eartquake;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
+import java.util.ArrayList;
 import com.example.app5eartquake.databinding.ActivityMainBinding;
 
-import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,43 +20,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        MainViewModel viewModel=new ViewModelProvider(this).get(MainViewModel.class);
 
         binding.eqRecycler.setLayoutManager(new LinearLayoutManager(this));
-        ArrayList<Earthquake> eqlist= new ArrayList<>();
-        eqlist.add(new Earthquake("001", "Carchi-Tulcan",5, 12082022, 100.5,154.8  ));
-        eqlist.add(new Earthquake("002", "Guayas-Guayaquil",6, 12082022, 100.5,154.8  ));
-        eqlist.add(new Earthquake("003", "Chimborazo- Alusi",3, 12082022, 100.5,154.8  ));
-        eqlist.add(new Earthquake("004", "Azuay-Cuenca",1, 12082022, 100.5,154.8  ));
-        eqlist.add(new Earthquake("005", "Azuay-Paute",4.3, 12082022, 100.5,154.8  ));
 
 
-        //Carga de datos
-        EqAdapter adapter = new EqAdapter();
-
-        adapter.setOnItemClickListener(earthquake -> abrir(earthquake.getId(),earthquake.getPlace(),earthquake.getMagnitude(),earthquake.getTime(),earthquake.getLatidude(),earthquake.getLongitude()));
-
-
+        EqAdapter adapter= new EqAdapter();
+        adapter.setOnItemClickListener(earthquake ->abrirVentana(earthquake.getId(),
+                earthquake.getPlace(),
+                earthquake.getMagnitude(),
+                earthquake.getTime(),
+                earthquake.getLatidude(),
+                earthquake.getLongitude()));
 
         binding.eqRecycler.setAdapter(adapter);
-        adapter.submitList(eqlist);
 
-        if(eqlist.isEmpty()){
-            binding.emptyView.setVisibility(View.VISIBLE);
-        }else{
-            binding.emptyView.setVisibility(View.GONE);
+        viewModel.getEqlist().observe(this, eqlist->{
+            adapter.submitList(eqlist);
 
-        }
-
+            if(eqlist.isEmpty()){
+                binding.emptyView.setVisibility(View.VISIBLE);
+            }else{
+                binding.emptyView.setVisibility(View.GONE);
+            }
+        });
+        viewModel.getEarthquakes();
     }
-    public void abrir (String id,String place,double magnitude, long time, double lat, double logi){
-        Toast.makeText(MainActivity.this,place, Toast.LENGTH_SHORT).show();
+    public void abrirVentana (String id,String place,double magnitude, long time, double lat, double logi){
+        //Toast.makeText(MainActivity.this,place, Toast.LENGTH_SHORT).show();
+        double longitud1=Math.round(logi*100.0)/100.0;
+        double latitud1=Math.round(lat*100.0)/100.0;
         Intent intent=new Intent(this,Monitor.class);
         intent.putExtra("id", id);
         intent.putExtra("place", place);
         intent.putExtra("magnitude", magnitude);
         intent.putExtra("time", time);
-        intent.putExtra("latitude", lat);
-        intent.putExtra("longitude", logi);
+        intent.putExtra("latitude", latitud1);
+        intent.putExtra("longitude", longitud1);
         startActivity(intent);
     }
 }
